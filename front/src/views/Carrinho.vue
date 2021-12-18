5f96569408f42656f2509e488f0f43b0c79f2277
jvlp <jvlp@cin.ufpe.br>
2021-12-18 15:51:10 -0300

    feat: confirmação de pagamento no carrinho

--- front/src/views/Carrinho.vue
+++ front/src/views/Carrinho.vue
@@ -22,7 +22,55 @@
                     </v-row>
                   </v-col>
                   <v-col cols="2" class="d-flex align-end justify-center">
-                    <v-btn class="mt-3" color="green" dark style="text">Confirmar</v-btn>
+                    <template>
+                      <div class="text-center">
+                        <v-btn
+                          color="deep-purple accent-4"
+                          class="white--text"
+                          @click="overlay = !overlay"
+                        >
+                          Confirmar
+                          <v-icon right>
+                            mdi-open-in-new
+                          </v-icon>
+                        </v-btn>
+                        <v-overlay :value="overlay">
+                          <v-progress-circular
+                            indeterminate
+                            size="64"
+                          ></v-progress-circular>
+                        </v-overlay>
+                      </div>
+                      <template>
+                        <v-row justify="space-around">
+                          <v-col cols="auto">
+                            <v-dialog
+                              overflow="hidden"
+                              v-if="showDialog" v-model="showDialog"
+                              transition="dialog-top-transition"
+                              max-width="600"
+                            >
+                              <template>
+                                <v-card style="overflow: hidden">
+                                  <v-row justify="center">
+                                    <v-icon size="200" color="green">mdi-check-bold</v-icon>
+                                  </v-row>
+                                  <v-card-text>
+                                    <div class="text-center text-h2 pa-12">Pagamento confirmado!</div>
+                                  </v-card-text>
+                                  <v-card-actions class="justify-end">
+                                    <v-btn
+                                      text
+                                      @click="dialog = false; goToPedidos()"
+                                    >Ok</v-btn>
+                                  </v-card-actions>
+                                </v-card>
+                              </template>
+                            </v-dialog>
+                          </v-col>
+                        </v-row>
+                      </template>
+                    </template>
                   </v-col>
                 </v-row>
               </v-card-text>
@@ -40,7 +88,26 @@ export default {
 
   data () {
     return {
+      overlay: false,
       carrinho: this.$store.state.carrinho
+    }
+  },
+
+  watch: {
+    overlay (val) {
+      setTimeout(() => {
+        this.showDialog = true
+      }, 950)
+      val &&
+      setTimeout(() => {
+        this.overlay = false
+      }, 1000)
+    }
+  },
+
+  methods: {
+    goToPedidos () {
+      this.$router.push({ path: '/pedidos', query: { pedido: 'aceito' } })
     }
   },
 
