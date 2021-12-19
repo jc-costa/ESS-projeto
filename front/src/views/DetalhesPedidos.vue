59fd63b47ec478658e6ff975ac1f28c9b0304480
jc-costa <jefficostau@gmail.com>
2021-12-19 00:08:41 -0300

    Fazendo conexão com o servidor

--- front/src/views/DetalhesPedidos.vue
+++ front/src/views/DetalhesPedidos.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container fluid fill-height>
     <v-row no-gutters justify="center" align="center">
-      <v-col lg=8>
-        <v-card class="rounded-xl">
+      <v-col lg=10 sm=10>
+        <v-card class="rounded-xl" max-height="600">
           <v-card-title>
             <v-btn icon @click="goBack()">
               <v-icon>mdi-chevron-left</v-icon>
@@ -10,20 +10,24 @@
             Pedido #{{this.$route.params.id}}
           </v-card-title>
           <v-card-text>
-            <div class="pl-10">
-              Data: {{this.date}}
-              <br>
-              Nome Restaurante: {{this.nomeRestaurante}}
-              <br>
-              <p class ="ml-6">Item(s):</p>
+            <v-col class="ml-10">
+              <v-row>
+                <p style="font-color: black">Data: {{this.date}}</p>
+              </v-row>
+              <v-row>
+                <p>Restaurante: {{this.nomeRestaurante}}</p>
+              </v-row>
+              <v-row>
+                <span class ="ml-6 mb-3">Item(s):</span>
+              </v-row>
               <!-- o v-for irá gerar essas tags de dentro do span, a quantidade de vezes q tem em informação. key  -->
-              <span v-for="(info, index) in getInfoPedidos" :key="index">
-              <p id="items">- {{info.descricao}}</p>
-              <br>
-              </span>
-              <br>
-              <span class="">Valor R$: {{this.valor}}</span>
-            </div>
+              <v-row v-for="(info, index) in itens" :key="index">
+                <p id="items">- {{info.descricao}} - R$ {{info.preco}}</p>
+              </v-row>
+              <v-row>
+                <p class="mt-8">Valor Total R$: {{this.valor}}</p>
+              </v-row>
+            </v-col>
           </v-card-text>
         </v-card>
       </v-col>
@@ -33,36 +37,40 @@
 
 <script>
 export default {
-  name: 'Detalhe Pedido',
+  name: 'DetalhePedido',
   data () {
     return {
       pedidos: this.$store.state.pedidos,
       id: this.$route.params.id,
+      itens: '',
       date: ' ',
       nomeRestaurante: ' ',
       valor: ' '
     }
   },
   computed: {
-    getInfoPedidos () {
-      const informacao = []
-      this.pedidos.forEach(pedido => {
-        pedido.infoPedidos.forEach(info => {
-          if (this.id === info.id) {
-            informacao.push(info)
-            this.date = pedido.dataPedido
-            this.nomeRestaurante = info.nomeRestaurante
-            this.valor = info.valor
-          }
-        })
-      })
-      return informacao
-    }
   },
   methods: {
     goBack () {
       this.$router.go(-1)
+    },
+    getInfoPedidos () {
+      console.log(this.pedidos.data)
+      this.pedidos.data.forEach(pedido => {
+        if (this.id === pedido.id.toString()) {
+          // console.log(pedido)
+          // console.log(new Date(pedido.data).toLocaleDateString('pt-BR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }))
+          this.date = new Date(pedido.data).toLocaleString('pt-BR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+          this.nomeRestaurante = pedido.restaurante.nome
+          this.valor = pedido.valorTotal
+          this.itens = pedido.itens
+        }
+      })
     }
+  },
+  created () {
+    console.log(this.$store.state.pedidos)
+    this.getInfoPedidos()
   }
 }
 </script>
